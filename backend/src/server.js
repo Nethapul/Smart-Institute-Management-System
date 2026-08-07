413f863c8aa2967a5f662532a6ecd7fd711deacf
Adeeksha Nethapul <adeekshanethapul111@gmail.com>
2026-08-07 04:29:05 +0400

    Connect Sequelize associations to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,22 @@
+// Load environment variables
+require("dotenv").config();
+
+
+// Load Sequelize model relationships
+require("./models/associations");
+
+
+// Load Express application
 const app = require("./app");
 
+
+// Server Port
 const PORT = process.env.PORT || 3000;
 
+
+// Start Server
 app.listen(PORT, () => {
+  console.log("=================================");
   console.log(`🚀 Server running on port ${PORT}`);
+  console.log("=================================");
 });
